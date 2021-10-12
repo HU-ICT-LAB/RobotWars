998626750ab342f538e53f3b889639f132ab66ac
Ruben van den Brink <rubenvandenbrink@hotmail.com>
2021-10-12 19:38:07 +0200

    remove unnecessary files and update colors

--- robotwarsreact/src/App.js
+++ robotwarsreact/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { MenuPage } from './pages/menu_page';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <MenuPage/>
     </div>
   );
 }
-
-export default App;
