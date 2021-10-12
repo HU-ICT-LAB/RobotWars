3ef93d9036c88c189a8dd13c3cf4489472f2baf0
Ruben van den Brink <rubenvandenbrink@hotmail.com>
2021-10-12 18:26:13 +0200

    Update api

--- RobotWarsApi/api.py
+++ RobotWarsApi/api.py
@@ -1,24 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 """
 Gets all robot ip's and returns them
 """
-@app.route("/robots", methods=["GET"])
+@app.route("/api/v1/robots", methods=["GET"])
 def get_robot_ips():
-    ips = []
     ##TODO implement functionality
-    return str(ips)
+    return jsonify(["192.168.0.1", "192.168.0.2"])
 
 
 """
 Starts all robots when /start is called
 """
-@app.route("/start", methods=["POST"])
+@app.route("/api/v1/start")
 def start_robots():
-    ##ToDo implement functionality
-    return "Robot started"
+    ##TODO implement functionality
+    return jsonify("Robot started")
 
 
 if __name__ == "__main__":
