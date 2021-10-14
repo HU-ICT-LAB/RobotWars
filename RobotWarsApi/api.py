51fe88c5ec57f43ec2fbe50a2f02e25a00dd43e6
Ruben van den Brink <rubenvandenbrink@hotmail.com>
2021-10-14 19:46:23 +0200

    Add good documentation

--- RobotWarsApi/api.py
+++ RobotWarsApi/api.py
@@ -1,22 +1,27 @@
+"""All api calls."""
 from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-"""
-Gets all robot ip's and returns them
-"""
+
 @app.route("/api/v1/robots", methods=["GET"])
 def get_robot_ips():
-    ##TODO implement functionality
+    """
+    Get all robot ip's.
+
+    :return: robot ip's
+    """
     return jsonify(["192.168.0.1", "192.168.0.2"])
 
 
-"""
-Starts all robots when /start is called
-"""
 @app.route("/api/v1/start")
 def start_robots():
-    ##TODO implement functionality
+    """
+    Start all robots.
+
+    :return: robot started if successfully started
+    """
     return jsonify("Robot started")
 
 
