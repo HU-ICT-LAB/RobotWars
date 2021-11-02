ac89ff7d291a802a069255a004d94ed08f6bdead
Max2411 <maxvankemenade@gmail.com>
2021-11-02 14:43:54 +0100

    Add qr connection script

---
+++ experiments/connection/qr.py
@@ -0,0 +1,26 @@
+import time
+from robomaster import conn
+from MyQR import myqr
+from PIL import Image
+import matplotlib.pyplot as plt
+import sys
+
+
+# Run this script in the console with the wifi and its password or
+# add the wifi and its password in the config of the qr
+ssid = sys.argv[1]
+password = sys.argv[2]
+
+
+if __name__ == 'main':
+    helper = conn.ConnectionHelper()
+    info = helper.build_qrcode_string(ssid=ssid, password=password)
+    myqr.run(words=info, save_name=f"{ssid}.png")
+    time.sleep(1)
+    img = Image.open(f"{ssid}.png")
+    plt.imshow(img)
+    plt.show()
+    if helper.wait_for_connection():
+        print("Connected!")
+    else:
+        print("Connect failed!")
