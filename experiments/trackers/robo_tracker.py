2c89556a9de6dcdbbde22d847f77f36d99c05369
Max2411 <maxvankemenade@gmail.com>
2021-11-05 13:37:42 +0100

    Added comments to code

--- experiments/trackers/robo_tracker.py
+++ experiments/trackers/robo_tracker.py
@@ -33,42 +33,50 @@ srobot.camera.start_video_stream(display=False, resolution="360p")
 srobot.vision.sub_detect_info(name="robot", callback=detect_robot)
 
 while True:
-
     img = srobot.camera.read_cv2_image(strategy="newest")
     img = imutils.resize(img, width=img_w, height=img_h)
 
-    for bounding_box in bounding_boxes:
-        x, y, w, h = bounding_box
+    # Draw bounding boxes.
+    for x, y, w, h in bounding_boxes:
         cv2.rectangle(img, (int((x - w / 2) * img.shape[1]),
                             int((y - h / 2) * img.shape[0])),
                       (int((x + w / 2) * img.shape[1]),
                        int((y + h / 2) * img.shape[0])),
                       (255, 0, 0), 2)
-    # set gimbal speed
+
     if len(bounding_boxes) > 0:
+        # Aims at the first bounding box in the list
         x, y, w, h = bounding_boxes[0]
         dx = x - 0.5
         dy = y - 0.5
         print(dx, dy)
         gvx, gvy = dx * gimbal_speed, -dy * gimbal_speed
+        # Set led to green when a robot is seen
         srobot.led.set_gimbal_led(r=0, g=255, b=0)
+        # Fire at the robot
         srobot.blaster.fire(fire_type=blaster.INFRARED_FIRE)
     else:
         gvx *= 0.7
         gvy *= 0.7
-
+        # Set led to red when no robot is seen
         srobot.led.set_gimbal_led(r=255, g=0, b=0)
-        # Shooting/ detecting laser
+
     srobot.gimbal.drive_speed(yaw_speed=gvx, pitch_speed=gvy)
 
     cv2.imshow(window_name, img)
     cv2.waitKey(1)
-
+    # Press [x] to stop the program
     if keyboard.is_pressed("x"):
         break
+
 cv2.destroyAllWindows()
+
+# Stops the gimbal and recenters it for future test
 srobot.gimbal.drive_speed(pitch_speed=0, yaw_speed=0)
 srobot.gimbal.recenter()
+
+# Stops info gathering and camera stream
+srobot.vision.unsub_detect_info()
 srobot.camera.stop_video_stream()
 
 srobot.close()
