e8af7c1e481a570a3d4fa27e87af91bf57625aae
Lucas van der Horst <Lucas@vdrHorst.nl>
2021-12-14 08:47:15 +0100

    - Fixed ray-intersection bug. - Implemented turret cooldown. - Implemented environment-object collision detection. - Added position to observation. - Visualize more things. - Fixed dependency versions.

--- ai_trainer/tank_trainer.py
+++ ai_trainer/tank_trainer.py
@@ -1,20 +1,27 @@
+import numpy as np
 import cv2
 from stable_baselines3 import PPO
 from tank_env import TankEnv
 
 env = TankEnv()
 
-# model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="runs")
-# model.learn(total_timesteps=3000000)
-# model.save("tank_policy")
+model = PPO("MlpPolicy", env, verbose=1)
+try:
+    model.learn(total_timesteps=30)
+except Exception as e:
+    print(e)
+finally:
+    model.save("tank_policy")
 
-model = PPO.load("tank_policy")
+model = PPO.load("tank_policy", env=env)
 window_name = "Tank Environment"
 cv2.namedWindow(window_name)
 obs = env.reset()
 while cv2.getWindowProperty(window_name, 0) >= 0:
-    action = model.predict(obs)
-    obs, reward, done, info = env.step(action[0])
+    # action = env.action_space.sample()
+    action = model.predict(obs)[0]
+    #action = np.array([0., 0., 0.1, 0., 0.1, 1.])
+    obs, reward, done, info = env.step(action)
     cv2.imshow(window_name, env.render())
     cv2.waitKey(round(env.step_size * 1000))
     if done:
