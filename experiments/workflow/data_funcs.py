2db08f510815394de8fca81301a5040d2f17e4e2
SwagLag <50711973+SwagLag@users.noreply.github.com>
2021-12-15 13:11:56 +0100

    Adds initial function (WIP), wiki picture for workflow

--- experiments/workflow/data_funcs.py
+++ experiments/workflow/data_funcs.py
@@ -12,3 +12,4 @@ def data_selection(samplesize: int, seed=None):
     host, user, password, db = config['MySQL']['host'], config['MySQL']['user'], config['MySQL']['passwd'], config['MySQL']['db_name']
     # Onthoud, queries alleen voor intern gebruik, buiten intern gebruik is dit een serieus veiligheidsrisico.
     conn = mysql.connector.connect(host, user, password, db)
+
