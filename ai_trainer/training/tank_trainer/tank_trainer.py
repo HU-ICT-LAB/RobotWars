d7e3ad84c6e164ce8cc0d34f98aefdfcbe82054a
Lucas van der Horst <Lucas@vdrHorst.nl>
2021-12-21 18:07:58 +0100

    Added Weights and Biases link

--- ai_trainer/training/tank_trainer/tank_trainer.py
+++ ai_trainer/training/tank_trainer/tank_trainer.py
@@ -1,14 +1,38 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
 import supersuit as ss
+import wandb
+from wandb.integration.sb3 import WandbCallback
 from ai_trainer.envs.tank_simulation.environment import TankEnv
 
-env = TankEnv()
-env = ss.frame_stack_v1(env, 3)
+config = {
+    'policy_type': 'MlpPolicy',
+    'total_timesteps': 5000000,
+}
+run = wandb.init(
+    project='Tank',
+    entity="robotwarshu",
+    config=config,
+    sync_tensorboard=True,
+    save_code=True,
+)
 
-model = PPO("MlpPolicy", env, verbose=1)
-try:
-    model.learn(total_timesteps=5000000)
-except Exception as e:
-    print(e)
-finally:
-    model.save("../../trained_policies/tank_policy")
+
+def make_env():
+    env = TankEnv()
+    env = ss.frame_stack_v1(env, 3)
+    return env
+
+
+env = DummyVecEnv([make_env])
+env = VecVideoRecorder(env, f"../../videos/{run.id}", record_video_trigger=lambda x: x % 2000 == 0, video_length=200)
+
+model = PPO(config['policy_type'], env, verbose=1, tensorboard_log=f"../../runs/{run.id}")
+model.learn(
+    total_timesteps=config['total_timesteps'],
+    callback=WandbCallback(
+        gradient_save_freq=100,
+        model_save_path=f"../../trained_policies/{run.id}",
+        verbose=2
+    ))
+run.finish()
