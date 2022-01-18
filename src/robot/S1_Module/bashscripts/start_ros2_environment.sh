657d74a62c3e248758d0e57c3cb5d41393d31343
MatthiesBrouwer <ties1000@live.nl>
2022-01-18 13:23:45 +0100

    restructured workspace

--- src/robot/S1_Module/bashscripts/start_ros2_environment.sh
+++ src/robot/S1_Module/bashscripts/start_ros2_environment.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+#Set the name of the docker image
 CONTAINER_IMAGE="jetson/ros25"
+
+#Set the name of the local directory you want to access within the image
+LOCAL_DIRECTORY=~/Documents/github/RobotWars/src/robot/
 
 #Give docker root user X11 permissions
 sudo xhost +si:localuser:root
@@ -14,5 +18,5 @@ chmod 777 $XAUTH
 sudo docker run -i -t --privileged -v /dev/input/:/dev/input/ --runtime nvidia --rm --network host -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
     -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \
-    -v ~/Documents/ws:/mnt \
+    -v $LOCAL_DIRECTORY:/mnt \
     $CONTAINER_IMAGE
