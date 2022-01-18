147cf8c6083810b5cbf697cdcab24a8d5123391b
unknown <rubenvandenbrink@hotmail.com>
2022-01-18 19:58:17 +0100

    add frontend implementation of get all robots

--- src/game_leader/robotwarsreact/src/components/robot_info/index.js
+++ src/game_leader/robotwarsreact/src/components/robot_info/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import SelectRobotBox from "../select_robot_box"
+import React, { useState, useEffect }from 'react';
 
 export default function RobotInfo() {
+    const [robots, setRobots] = useState([]);
+    let [selectedRobot, setSelectedRobot] = useState(null);
+
+    useEffect(() => {
+      fetch('http://127.0.0.1:5000/api/v1/robots')
+      .then(response => response.json())
+      .then(
+        (data) => {
+        setRobots(data[0])
+        setSelectedRobot(data[0][0])
+      },
+      (error) => {
+        window.alert(error)
+      })
+    }, [])
+
+    function handleChange(event) {
+        setSelectedRobot(robots.find(robot => robot.name === event.target.value))
+    }
     return(
         <div style={styles.robotInfo}>
-            <SelectRobotBox/>
+            <select style={styles.select} onChange={handleChange}>
+                {robots?.map((item)=> {
+                            return <option value={item.name}>{item.name}</option>
+                        })}
+            </select>
             <div style={styles.fields}>
-                <text style={styles.field}>Name:</text>
-                <text style={styles.field}>HP:</text>
-                <text style={styles.field}>Description:</text>
-                <text style={styles.field}>Location:</text>
+                <text style={styles.field}>{"Name: " + (selectedRobot?.name === null ? "" : selectedRobot?.name)}</text>
+                <text style={styles.field}>{"HP: " + (selectedRobot?.hp === null ? "" : selectedRobot?.hp)}</text>
+                <text style={styles.field}>{"Description: " + (selectedRobot?.description === null ? "" : selectedRobot?.description)}</text>
+                <text style={styles.field}>{"Location: " + (selectedRobot?.location === null ? "" : selectedRobot?.location)}</text>
             </div>
         </div>
     )
@@ -24,6 +46,11 @@ const styles = {
         minWidth: "500px",
         display: "flex",
         flexDirection: "column",
+        borderRadius: "10px"
+      },
+      select: {
+        width: "200px",
+        height: "20px",
         borderRadius: "10px"
       },
     fields: {
--- src/game_leader/robotwarsreact/src/pages/game_overview_page.js
+++ src/game_leader/robotwarsreact/src/pages/game_overview_page.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Header from "../components/header"
 
-
 export default function GameOverviewPage() {
     const [games, setGames] = useState([]);
     let [selectedGame, setSelectedGame] = useState(null);
@@ -33,14 +32,14 @@ export default function GameOverviewPage() {
                             return <option value={item.name}>{item.name}</option>
                         })}
                     </select>
-                    <text style={styles.field}>{"Name: " + selectedGame?.name}</text>
-                    <text style={styles.field}>{"Description: " + selectedGame?.description}</text>
-                    <text style={styles.field}>{"Gamemode: " + selectedGame?.gameMode}</text>
-                    <text style={styles.field}>{"Robot 1 Team: " + selectedGame?.robot1Team}</text>
-                    <text style={styles.field}>{"Robot 2 Team: " + selectedGame?.robot2Team}</text>
-                    <text style={styles.field}>{"Robot 3 Team: " + selectedGame?.robot3Team}</text>
-                    <text style={styles.field}>{"Maximum hp of robots: " + selectedGame?.maxHp}</text>
-                    <text style={styles.field}>{"Game Time: " + selectedGame?.gameTime}</text>
+                    <text style={styles.field}>{"Name: " + (selectedGame?.name === null ? "" : selectedGame?.name)}</text>
+                    <text style={styles.field}>{"Description: " + (selectedGame?.description === null ? "" : selectedGame?.description)}</text>
+                    <text style={styles.field}>{"Gamemode: " + (selectedGame?.gameMode === null ? "" : selectedGame?.gameMode)}</text>
+                    <text style={styles.field}>{"Robot 1 Team: " + (selectedGame?.robot1Team === null ? "" : selectedGame?.robot1Team)}</text>
+                    <text style={styles.field}>{"Robot 2 Team: " + (selectedGame?.robot2Team === null ? "" : selectedGame?.robot2Team)}</text>
+                    <text style={styles.field}>{"Robot 3 Team: " + (selectedGame?.robot3Team === null ? "" : selectedGame?.robot3Team)}</text>
+                    <text style={styles.field}>{"Maximum hp of robots: " + (selectedGame?.maxHp === null ? "" : selectedGame?.maxHp)}</text>
+                    <text style={styles.field}>{"Game Time: " + (selectedGame?.gameTime === null ? "" : selectedGame?.gameTime)}</text>
                 </div>
             </div>
         </div>
