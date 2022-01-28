409f74ee6874a7ec928e97126c112eb2fd5b40eb
MatthiesBrouwer <ties1000@live.nl>
2022-01-28 15:50:48 +0100

    fixing flake8

--- src/robot/S1_Module/S1_ws/launch/S1__full_launch.launch.py
+++ src/robot/S1_Module/S1_ws/launch/S1__full_launch.launch.py
@@ -1,13 +1,14 @@
+""""Launch file to start all nodes containes in packages 'teleop_joy' and 'S1_Master'"""
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-
 def generate_launch_description():
+    """The launch description function"""
     S1_driver_1 = IncludeLaunchDescription(
+
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('S1_master'), 'launch'),
             '/launch_S1_drivers__1.launch.py'])
@@ -18,7 +19,6 @@ def generate_launch_description():
             get_package_share_directory('teleop_joy'), 'launch'),
             '/launch_teleop_joy_twist.launch.py'])
     )
-
 
     return LaunchDescription([
         S1_driver_1,
